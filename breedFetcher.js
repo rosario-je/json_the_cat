c6b888030f862cc4dd504ae191ed156bf54b1800
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-24 16:06:15 -0500

    feat: Add logic to display cat description feat: Add edge case where breed is not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,5 +1,6 @@
 const request = require('request');
-const readline = require('readline')
+const readline = require('readline');
+const { log } = require('console');
 
 let rl = readline.createInterface({
   input: process.stdin,
@@ -7,14 +8,33 @@ let rl = readline.createInterface({
 });
 
 
-rl.question('Enter a breed name:', function(answer) {
+rl.question('Enter a breed name:', function (answer) {
+  //Api link with user input
   const api = `https://api.thecatapi.com/v1/breeds/search?q=${answer}`
-  request(api, (error, response, data) => {
+  //Create https request of API link
+  request(api, (error, response, body) => {
+    //Log error if error occurs 
     if (error) console.log(error);
-  
+
     console.log(response.statusCode);
-  
-    console.log(typeof data)
-  
+    //console.log('This is the body:', body);
+
+
+    //Parse body to turn string into JSON 
+    const data = JSON.parse(body)
+
+    // console.log(typeof data);
+    //Check if breed exists 
+    if (data.length == 0) {
+      console.log('Breed not found');
+      process.exit();
+    }
+
+    //console.log(data);
+
+    //Log cat description
+    console.log(data[0].description);
+    process.exit();
+
   })
 })
